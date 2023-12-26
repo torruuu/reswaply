98082af6df77b520de072cfa11cc72a1fa6b57d8
Alejandro <ale.torrubiano@gmail.com>
2023-12-26 17:00:14 +0100

    Refactorizada la base de datos para facilitar las opciones de filtrado de la app

--- src/routes/profile/products/+page.server.js
+++ src/routes/profile/products/+page.server.js
@@ -1,12 +1,8 @@
 export async function load({ cookies }) {
     const userName = cookies.get('user');
 
-    const response1 = await fetch("http://localhost:4000/users?name=" + userName);
-    const userJson = await response1.json();
-    const userId = userJson[0].id;
-
-    const response2 = await fetch("http://localhost:4000/users/" + userId + "/posts");
-    const userPosts = await response2.json();
+    const response = await fetch("http://localhost:4000/posts?seller=" + userName);
+    const userPosts = await response.json();
 
     return { userPosts }
 }
