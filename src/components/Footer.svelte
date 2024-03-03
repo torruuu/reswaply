202cdede4df4c4980dca97d3062c8dfbd6843209
Alejandro <ale.torrubiano@gmail.com>
2024-03-03 22:03:02 +0100

    Realizado diseño del footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,12 +1,85 @@
 <div class="container">
-
+    <div class="columns">
+        <div class="logo">
+            <div class="logo-content">
+                <img src="/images/logo-reswaply.png" alt="Logo">
+                <span class="title">Swaply</span>
+            </div>
+            <span>Copyright © 2024 ReSwaply © de sus respectivos propietarios</span>
+        </div>
+        <div class="about column">
+            <h5>Sobre ReSwaply</h5>
+            <span>¿Quiénes somos?</span>
+            <span>Reventa</span>
+            <span>Descuento estudiantes</span>
+            <span>Prensa</span>
+        </div>
+        <div class="help column">
+            <h5>Ayuda</h5>
+            <span>Profesionales:¿cómo vender aquí?</span>
+            <span>Pago</span>
+            <span>Entrega</span>
+            <span>Devoluciones y Reembolsos</span>
+            <span>Seguros</span>
+            <span>Atención al cliente</span>
+        </div>
+        <div class="legal column">
+            <h5>Condiciones legales</h5>
+            <span>Condiciones generales de uso</span>
+            <span>Condiciones generales de venta</span>
+            <span>Garantía comercial</span>
+            <span>Protección de datos</span>
+            <span>Cookies</span>
+            <span>Aviso legal</span>
+        </div>
+    </div>
 </div>
 
 <style>
     .container {
+        padding-top: 3rem;
         border-top: 0.1rem solid rgb(205, 205, 205);
         background-color: #fff;
         max-width: 100%;
         height: 20rem;
+        display: flex;
+        justify-content: center;
+    }
+
+    .columns {
+        width: 80%;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        gap: 5rem;
+    }
+
+    .logo-content {
+        display: flex;
+        align-items: center;
+        margin-bottom: 0.4rem;
+    }
+
+    .title {
+        font-size: 2rem;
+        font-weight: 600;
+    }
+
+    .column {
+        display: flex;
+        flex-direction: column;
+        gap: 0.6rem;
+    }
+
+    img {
+        width: 2rem;
+    }
+
+    h5 {
+        margin: 0;
+        font-size: 1.4rem;
+    }
+
+    span {
+        font-size: 0.9rem;
     }
 </style>
